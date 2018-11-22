736eeb859fbdbc66ceca5441f348331a32b3e79d
Gubernik Vladislav <vgubernik@gmail.com>
2018-11-22 16:50:54 +0300

    Initial commit

---
+++ src/main/java/ru/gubernik/CompanyBase/CompanyBaseApplication.java
@@ -0,0 +1,12 @@
+package ru.gubernik.CompanyBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyBaseApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/gubernik/CompanyBase/CompanyBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.gubernik.CompanyBase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
