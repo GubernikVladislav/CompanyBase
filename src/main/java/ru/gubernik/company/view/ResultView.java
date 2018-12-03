27e64532d2bc9579416403b6703c299fa3e36ca0
Gubernik Vladislav <vgubernik@gmail.com>
2018-12-03 14:47:40 +0300

    Изменена реализация класса ResultView

--- src/main/java/ru/gubernik/company/view/ResultView.java
+++ src/main/java/ru/gubernik/company/view/ResultView.java
@@ -1,9 +1,14 @@
 package ru.gubernik.company.view;
 
+/**
+ * Представление сообщения результата
+ */
 public class ResultView {
+
     String result;
 
-    ResultView(){
-        result = "succes";
+    @Override
+    public String toString(){
+        return result = "succes";
     }
 }
