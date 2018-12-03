450abf238d5bf722ba5e77f12e1e1f98982027ab
Gubernik Vladislav <vgubernik@gmail.com>
2018-12-03 14:57:17 +0300

    Создан Dao для работы с Organization

---
+++ src/main/java/ru/gubernik/company/dao/organization/OrganizationDao.java
@@ -0,0 +1,37 @@
+package ru.gubernik.company.dao.organization;
+
+import ru.gubernik.company.model.Organization;
+
+import java.util.List;
+
+/**
+ * DAO для работы с Organization
+ */
+public interface OrganizationDao {
+
+    /**
+     * Получить список организаций
+     * @return List список организаций
+     */
+    List<Organization> all();
+
+    /**
+     * Получить организацю по id
+     * @param id - идентификатор организации
+     * @return возвращает организацию
+     */
+    Organization loadById(Integer id);
+
+    /**
+     * Обновление организации по id
+     * @param id идентификатор организации
+     * @param organization обновленная организация
+     */
+    void updateById(Integer id, Organization organization);
+
+    /**
+     * Добавление организации
+     * @param organization организация
+     */
+    void save(Organization organization);
+}
---
+++ src/main/java/ru/gubernik/company/dao/organization/OrganizationDaoImpl.java
@@ -0,0 +1,51 @@
+package ru.gubernik.company.dao.organization;
+
+
+import ru.gubernik.company.model.Organization;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+/**
+ * Реализация интерфейса OrganizationDao
+ */
+public class OrganizationDaoImpl implements OrganizationDao {
+
+    private final EntityManager entityManager;
+
+    public OrganizationDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Organization> all() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Organization loadById(Integer id) {
+        return entityManager.find(Organization.class, id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateById(Integer id, Organization organization) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void save(Organization organization) {
+        entityManager.persist(organization);
+    }
+}
