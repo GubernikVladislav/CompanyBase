00ed9b13ae210bdae3bd96a95438f2982b005eb5
Gubernik Vladislav <vgubernik@gmail.com>
2018-12-06 00:19:03 +0300

    Добавлена валидация

--- src/main/java/ru/gubernik/company/view/country/CountryView.java
+++ src/main/java/ru/gubernik/company/view/country/CountryView.java
@@ -1,5 +1,8 @@
 package ru.gubernik.company.view.country;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Представление страны
  */
@@ -8,11 +11,15 @@ public class CountryView {
     /**
      * Название страны
      */
+    @NotNull(message = "name cannot be null")
+    @Size(max = 255)
     public String name;
 
     /**
      * Код страны
      */
+    @NotNull(message = "code cannot be null")
+    @Size(max = 3)
     public String code;
 
     @Override
